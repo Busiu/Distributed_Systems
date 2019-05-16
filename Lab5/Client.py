7041d8a1e79e2d40b3814618bee5b00b7909728a
Busiu <busiu69@gmail.com>
2019-05-16 22:01:04 +0200

    Lab5 - Added book_searching functionality.

--- Lab5/Client.py
+++ Lab5/Client.py
@@ -3,8 +3,12 @@ import pykka
 from Server import Server
 
 if __name__ == "__main__":
-    server = Server.start()
-    response = server.ask("WOW")
-    print(response)
-    response = server.ask("Hello")
-    print(response)
+    server = Server.start().proxy()
+    result = server.book_searching("Attack on Titan Vol 4").get()
+    print(result)
+    result = server.book_searching("Attack on Titan Vol 20").get()
+    print(result)
+    result = server.book_searching("Peter fights Hornet").get()
+    print(result)
+    result = server.book_searching("Jezioro labedzie").get()
+    print(result)
